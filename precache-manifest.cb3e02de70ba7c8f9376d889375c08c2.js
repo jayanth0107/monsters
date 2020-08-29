7ee0d7765e28627a0ed0bf12c990dc7be55bb848
jayanth0107 <bujji.0107@gmail.com>
2020-08-29 16:52:40 +0530

    Updates

---
+++ precache-manifest.cb3e02de70ba7c8f9376d889375c08c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd2c60f22658d73a8ad75d5750ff7d2",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "6ce84d6d6aa988a2ce51",
+    "url": "/monsters/static/css/main.f65e1830.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce84d6d6aa988a2ce51",
+    "url": "/monsters/static/js/main.199b0cca.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
